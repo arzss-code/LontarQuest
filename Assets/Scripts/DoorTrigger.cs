44a24727f15382e927d00022d3a6c791626c7aa8
Wrseno <warsenosetyono@gmail.com>
2026-06-05 08:14:59 +0700

    feat: add cutscene

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private string nextSceneName = "SampleScene";
+
+    private bool canEnter = false;
+
+    public void EnableDoor()
+    {
+        canEnter = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!canEnter)
+            return;
+
+        if (!other.CompareTag("Player"))
+            return;
+
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
